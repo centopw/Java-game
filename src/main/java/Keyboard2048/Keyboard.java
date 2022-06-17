0040f1a5b2681be6edeb260461b2850d1c3a5624
pietyfilial <trannguyenhieutrung@gmail.com>
2022-06-17 14:57:30 +0700

    Update keyboard

--- src/main/java/Keyboard2048/Keyboard.java
+++ src/main/java/Keyboard2048/Keyboard.java
@@ -4,10 +4,48 @@
  */
 package Keyboard2048;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  *
- * @author ACER
+ * @author Trung.
  */
-public class Keyboard {
+public class Keyboard implements KeyListener{
     
+    public static boolean[] keys = new boolean[120];
+    public static boolean[] lastKeys = new boolean[120];
+    
+    public void update(){
+        for(int i = 0; i < keys.length; i++){
+            lastKeys[i] = keys[i];
+        }
+    }
+    
+    public static boolean key(int key){
+        return keys[key];
+    }
+    
+    public static boolean keyDown(int key){
+        return keys[key] && !lastKeys[key];
+    }
+    
+    public static boolean keyUp(int key){
+        return !keys[key] && lastKeys[key];
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+   }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+   }
 }
