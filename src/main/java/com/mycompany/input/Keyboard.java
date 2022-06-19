4fe737fe951684af281d4ade1c4d6579b7b2a11b
Cento <tanhiep26@gmail.com>
2022-06-19 21:22:08 +0700

    Reorder and fix bugs

---
+++ src/main/java/com/mycompany/input/Keyboard.java
@@ -0,0 +1,47 @@
+package com.mycompany.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author Trung.
+ */
+public class Keyboard implements KeyListener{
+    
+    public static boolean[] keys = new boolean[120];
+    public static boolean[] lastKeys = new boolean[120];
+    
+    public void update(){
+        for(int i = 0; i < keys.length; i++){
+            lastKeys[i] = keys[i];
+        }
+    }
+    
+    public static boolean key(int key){
+        return keys[key];
+    }
+    
+    public static boolean keyDown(int key){
+        return keys[key] && !lastKeys[key];
+    }
+    
+    public static boolean keyUp(int key){
+        return !keys[key] && lastKeys[key];
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+   }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+   }
+}
